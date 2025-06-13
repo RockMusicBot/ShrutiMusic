afcd3d6a85dd2cfa2589318b7a06a5d97339d7c5
RockMusicBot <githubmusicbot@gmail.com>
2025-06-13 22:48:42 +0530

    __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import GroupCallNotFoundError
+from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from ShrutiMusic import LOGGER, app, userbot
@@ -23,9 +23,7 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
-
     await sudo()
-
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -33,36 +31,28 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except Exception as e:
-        LOGGER("ShrutiMusic").warning(f"Error fetching banned users: {e}")
-
+    except:
+        pass
     await app.start()
-
     for all_module in ALL_MODULES:
         importlib.import_module("ShrutiMusic.plugins" + all_module)
     LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
-
     await userbot.start()
     await Aviax.start()
-
     try:
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except GroupCallNotFoundError:
+    except NoActiveGroupCall:
         LOGGER("ShrutiMusic").error(
-            "Please start the video chat in your log group/channel first.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
-    except Exception as e:
-        LOGGER("ShrutiMusic").error(f"Stream failed: {e}")
-        exit()
-
+    except:
+        pass
     await Aviax.decorators()
     LOGGER("ShrutiMusic").info(
-        "Shruti Music Started Successfully with PyTgCalls 2.x!\nVisit @ShrutiBots"
+        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
     )
-
     await idle()
-
     await app.stop()
     await userbot.stop()
     LOGGER("ShrutiMusic").info("Stopping Shruti Music Bot...")
